cf6423b537243f3d4da49e96c57418079a214bb2
kimkieu1197 <53105543+kimkieu1197@users.noreply.github.com>
2019-07-29 10:54:55 +0700

    đã làm được được rồi hhhhh

---
+++ kimkieu123/kimkieu123/gestbook.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="gestbook.aspx.cs" Inherits="kimkieu123.gestbook" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:Label ID="lblkk" runat="server" ForeColor="#FF3300" Text="GESTBOOK"></asp:Label>
+        <br />
+    <div>
+    
+        <asp:Label ID="lbltieude" runat="server" Text="Tiêu đề:"></asp:Label>
+        <asp:TextBox ID="txttieude" runat="server" style="margin-left: 55px" Width="177px"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="lblhoten" runat="server" Text="Họ tên:"></asp:Label>
+        <asp:TextBox ID="txthoten" runat="server" Height="22px" style="margin-left: 58px" Width="179px"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="lblemail" runat="server" Text="Email:"></asp:Label>
+        <asp:TextBox ID="txtemail" runat="server" style="margin-left: 67px" Width="176px"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="lblnoidung" runat="server" Text="Nội dung:"></asp:Label>
+        <asp:TextBox ID="txtnoidung" runat="server" style="margin-left: 42px" Width="183px"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="btngnd" runat="server" OnClick="btngnd_Click" style="margin-left: 286px" Text="Gởi nội dung" Width="279px" />
+        <br />
+        <asp:Button ID="btndscm" runat="server" Text="DANH SÁCH COMMENT" Width="923px" />
+        <br />
+        <span id="Entrycoment" runat="server"/>
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ kimkieu123/kimkieu123/gestbook.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace kimkieu123
+{
+    public partial class gestbook : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ReadComment();
+        }
+
+        protected void btngnd_Click(object sender, EventArgs e)
+        {
+            //lưu nội dung comment mới vào file
+            //tạo file theo đường dẫn của website
+            string sfile = Server.MapPath(@"\") + "data.txt";
+            //mở file nếu khác null(mở được) thì bắt đầu thêm
+            using (StreamWriter Writer = new StreamWriter(sfile, true))
+            {
+                //Thêm các thông tin của entry comment mới vào
+                Writer.WriteLine(txttieude.Text);
+                Writer.WriteLine(txthoten.Text);
+                Writer.WriteLine(txtemail.Text);
+                Writer.WriteLine(txtnoidung.Text);
+                //Thêm kí hiệu nhận dạng kết thúc một entry->để sau này lọc
+                //ra từng entry
+                Writer.WriteLine("#END");
+                Writer.Close();//đóng file sau khi gửi
+
+            }
+
+
+        }
+        private void ReadComment()
+        {
+            //hàm đọc tất cả các comment và đưa lleen table trên web
+            string file = Server.MapPath(@"\") + "data.txt";
+            //mở file nếu khác null thì bắt đầu đọc
+            using (StreamReader reader=new StreamReader(file))
+            {
+                string noidung = reader.ReadToEnd();//đọc toanf bộ file
+                //sử dụng mảng chuổi kết thúc entry
+                string[] delimiter = { "#END" };
+                //lọc ra từng entry và đưa vào mảng chuổi
+                string[] arr=noidung.Split(delimiter,
+                   StringSplitOptions.RemoveEmptyEntries );
+
+                foreach (string s in arr)//duyệt va từng entry
+                {
+                    string stemp;
+                    //thay thế kí tự xuống dòng bằng dấu dòng bằng cách tạo break
+                    stemp = Regex.Replace(s,@"\r\n",@"<br/>");
+                    string entry = string.Format("<tr><td colspan=\"2\"{0} </td></tr>",stemp);
+                   Entrycoment.InnerHtml += entry;
+                }
+
+
+            }
+        }
+    }
+}
